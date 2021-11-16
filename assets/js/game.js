5d817943ca60960398eff07b667934775d350ae6
Chewbakkah <chewbakkah@gmail.com>
2021-11-16 15:19:05 -0600

    added random values generator

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = Math.floor(Math.random() * 60);
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
@@ -23,14 +23,17 @@ var fight = function (enemyName) {
 			if (confirmSkip) {
 				window.alert(playerName + " has decided to skip this fight. Goodbye!");
 				// subtract money from playerMoney for skipping
-				playerMoney = playerMoney - 10;
+				playerMoney = Math.max(0, playerMoney - 10);
 				console.log("playerMoney", playerMoney);
 				break;
 			}
 		}
 
 		// remove enemy's health by subtracting the amount set in the playerAttack variable
-		enemyHealth = enemyHealth - playerAttack;
+		// generate random damage value based on player's attack power
+		var damage = randomNumber(playerAttack - 3, playerAttack);
+
+		enemyHealth = Math.max(0, enemyHealth - damage);
 		console.log(
 			playerName +
 				" attacked " +
@@ -56,7 +59,9 @@ var fight = function (enemyName) {
 		}
 
 		// remove players's health by subtracting the amount set in the enemyAttack variable
-		playerHealth = playerHealth - enemyAttack;
+		var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+		playerHealth = Math.max(0, playerHealth - damage);
 		console.log(
 			enemyName +
 				" attacked " +
@@ -95,7 +100,7 @@ var startGame = function () {
 			var pickedEnemyName = enemyNames[i];
 
 			// reset enemyHealth before starting new fight
-			enemyHealth = 50;
+			enemyHealth = randomNumber(40, 60);
 
 			// pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
 
@@ -183,6 +188,13 @@ var shop = function () {
 			shop();
 			break;
 	}
+};
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+	var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+	return value;
 };
 
 startGame();
